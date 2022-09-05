d9b0c9dda04cf6aa2e3285b072a37dbc5b01fb80
xieby1 <xieby1@outlook.com>
2022-09-06 01:09:09 +0800

    generating dot works great

--- scripts/data.py
+++ scripts/data.py
@@ -1,44 +1,82 @@
 #!/usr/bin/env python3
 from lib import *
 
-qemu_user_i = {
+# https://github.com/torvalds/linux/tree/master/arch
+Isa_LINUXs = {Isa.ALPHA, Isa.ARC} | Isa_ARM32s | {Isa.ARM64,
+    Isa.CSKY, Isa.HEXAGON, Isa.IA64, Isa.LOONGARCH, Isa.M68K,
+    Isa.MICROBLAZE} | Isa_MIPS64s | {Isa.NIOS2, Isa.OPENRISC,
+    Isa.PARISC} | Isa_POWERPC64s | Isa_RISCV64s | {Isa.S390,
+    Isa.SH} | Isa_SPARC64s | Isa_X86_64s | {Isa.XTENSA}
+Module(Kernel.LINUX.name, set(
+    IO(isa.name, Metaface({isa}), Metaface({isa}, {Kernel.LINUX}))
+for isa in Isa_LINUXs))
+
+# https://www.freebsd.org/platforms/
+Isa_BSDs = Isa_X86_64s | Isa_X86s | {Isa.AARCH64, Isa.ARM} | \
+    Isa_ARMV6s | Isa_ARMV7s | Isa_MIPS64s | Isa_POWERPC64s | \
+    Isa_RISCV64s | Isa_SPARC64s
+# https://www.openbsd.org/plat.html
+Isa_BSDs |= {Isa.ALPHA, Isa.HPPA}
+Module(Kernel.BSD.name, set(
+    IO(isa.name, Metaface({isa}), Metaface({isa}, {Kernel.BSD}))
+for isa in Isa_BSDs))
+
+#Transor("",
+#    {  IO("",
+#        Metaface(),
+#        Metaface(),
+#    )},
+#    Date(),
+#)
+Transor("Merge",
+    {  IO("",
+        Metaface({Isa.I386}, {Kernel.SCO_UNIX}),
+        Metaface({Isa.I386}, {Kernel.DOS, Kernel.WINDOWS3_1})
+    )},
+    Date(1985,10,9), Date(2000),
+    desc="https://en.wikipedia.org/wiki/Merge_(software)"
+)
+Transor("mx",
+    {  IO("",
+        Metaface({Isa.ALPHA}, {Kernel.OSF1}),
+        Metaface({Isa.MIPS}, {Kernel.ULTRIX}),
+    )},
+    Date(1993), dev="Digital",
+    desc="1993: Binary Translation by Richard L. Sites"
+)
+
+Isa_QEMU_user_is = {
     Isa.AARCH64, Isa.ARM, Isa.X86, Isa.X86_64,
     Isa.LA64, Isa.MIPS,
     Isa.POWERPC, Isa.RISCV64, Isa.S390X, Isa.SPARC,
 }
-qemu_user_o = {
+Isa_QEMU_user_os = {
     Isa.ALPHA, Isa.ARM, Isa.AARCH64, Isa.X86, Isa.X86_64,
     Isa.AVR, Isa.CRIS, Isa.HEXAGON, Isa.HPPA, Isa.M68K,
     Isa.MICROBLAZE, Isa.MIPS, Isa.MIPS64, Isa.NIOS2,
     Isa.OPENRISC, Isa.POWERPC, Isa.RISCV64, Isa.RX,
     Isa.S390X, Isa.SH4, Isa.SPARC, Isa.TRICORE, Isa.XTENSA,
 }
-Module(
-    # name
-    "QEMU-user",
-    # ios: set[IO]
-    {  IO(
-        "linux",
-        Metaface(qemu_user_i, {Kernel.LINUX}, Syslib_ANYs, Lib_ANYs),
-        Metaface(qemu_user_o, {Kernel.LINUX})
-    ), IO(
-        "bsd",
-        Metaface(qemu_user_i, {Kernel.BSD}, Syslib_ANYs, Lib_ANYs),
-        Metaface(qemu_user_o, {Kernel.BSD})
-    ), IO(
-        "tci-linux",
-        Metaface(Isa_ANYs, {Kernel.LINUX}, Syslib_ANYs, Lib_ANYs),
-        Metaface(qemu_user_o, {Kernel.LINUX})
-    ), IO(
-        "tci-bsd",
-        Metaface(Isa_ANYs, {Kernel.BSD}, Syslib_ANYs, Lib_ANYs),
-        Metaface(qemu_user_o, {Kernel.BSD})
+Transor("QEMU-user",
+    {  IO("linux",
+        Metaface(Isa_QEMU_user_is & Isa_LINUXs, {Kernel.LINUX}, Syslib_ANYs, Lib_ANYs),
+        Metaface(Isa_QEMU_user_os & Isa_LINUXs, {Kernel.LINUX})
+    ), IO("bsd",
+        Metaface(Isa_QEMU_user_is & Isa_BSDs, {Kernel.BSD}, Syslib_ANYs, Lib_ANYs),
+        Metaface(Isa_QEMU_user_os & Isa_BSDs, {Kernel.BSD})
+    ), IO("tci-linux",
+        Metaface(Isa_LINUXs, {Kernel.LINUX}, Syslib_ANYs, Lib_ANYs),
+        Metaface(Isa_QEMU_user_os & Isa_LINUXs, {Kernel.LINUX})
+    ), IO("tci-bsd",
+        Metaface(Isa_BSDs, {Kernel.BSD}, Syslib_ANYs, Lib_ANYs),
+        Metaface(Isa_QEMU_user_os & Isa_BSDs, {Kernel.BSD})
     )},
-    Date(2003,2),
-    Date.today(),
-    "#F60",
-    "IR",
+    Date(2003,2), Date.today(), "#F60", "IR",
     "2005: QEMU, a Fast and Portable Dynamic Translator",
 )
 
-printGnucladCsv()
+addDummyModules()
+
+# printGnucladCsv()
+printDot()
+# printEdges()
--- scripts/lib.py
+++ scripts/lib.py
@@ -2,34 +2,42 @@
 # https://stackoverflow.com/questions/33533148/
 from __future__ import annotations
 import enum
+import warnings
 
 class App(enum.Enum):
     NONE = enum.auto()
-    ANY = enum.auto()
+    APPS = ANY = enum.auto()
     END = enum.auto()
 App_ANYs = set(App(i) for i in range(App.ANY.value, App.END.value))
 
 class Sysapp(enum.Enum):
     NONE = enum.auto()
-    ANY = enum.auto()
+    SYSAPPS = ANY = enum.auto()
     END = enum.auto()
 Sysapp_ANYs = set(Sysapp(i) for i in range(Sysapp.ANY.value, Sysapp.END.value))
 
 class Lib(enum.Enum):
     NONE = enum.auto()
-    ANY = enum.auto()
+    LIBS = ANY = enum.auto()
     END = enum.auto()
 Lib_ANYs = set(Lib(i) for i in range(Lib.ANY.value, Lib.END.value))
 
 class Syslib(enum.Enum):
     NONE = enum.auto()
-    ANY = enum.auto()
+    SYSLIBS = ANY = enum.auto()
     END = enum.auto()
 Syslib_ANYs = set(Syslib(i) for i in range(Syslib.ANY.value, Syslib.END.value))
 
 class Kernel(enum.Enum):
     NONE = enum.auto()
+    KERNELS = ANY = enum.auto()
+    SCO_UNIX = enum.auto()
+    TRU64 = OSF1 = DIGITAL_UNIX = enum.auto()
+    ULTRIX = enum.auto()
+    UNIX = enum.auto()
     LINUX = enum.auto()
+    DOS = enum.auto()
+    WINDOWS3_1 = enum.auto()
     WINDOWS = enum.auto()
     MACOS = enum.auto()
     BSD = enum.auto()
@@ -37,7 +45,7 @@ class Kernel(enum.Enum):
 
 class Isa(enum.Enum):
     NONE = enum.auto()
-    ANY = enum.auto()
+    ISAS = ANY = enum.auto()
     I386 = enum.auto()
     I486 = enum.auto()
     I586 = enum.auto()
@@ -73,8 +81,8 @@ class Isa(enum.Enum):
 
     # TODO: from qemu tcg
 
-    LA = LA64 = enum.auto()
-    S390X = enum.auto()
+    LA = LA64 = LOONGARCH = LOONGARCH64 = enum.auto()
+    S390 = S390X = enum.auto()
 
     # TODO: from qemu targets
 
@@ -88,15 +96,27 @@ class Isa(enum.Enum):
     NIOS2 = enum.auto()
     OPENRISC = enum.auto()
     RX = enum.auto()
-    SH4 = enum.auto()
+    SH = SH4 = enum.auto()
     TRICORE = enum.auto()
     XTENSA = enum.auto()
 
+    # TODO: from linux arch
+    ARC = enum.auto()
+    CSKY = enum.auto()
+    IA64 = enum.auto()
+    PARISC = enum.auto()
+
     END = enum.auto()
 Isa_ANYs = set(Isa(i) for i in range(Isa.ANY.value, Isa.END.value))
-Isa_X86 = set(Isa(i) for i in range(Isa.I386.value, Isa.X86.value+1))
+Isa_X86s = set(Isa(i) for i in range(Isa.I386.value, Isa.X86.value+1))
 Isa_X86_64s = set(Isa(i) for i in range(Isa.I386.value, Isa.X86_64.value+1))
 Isa_ARM32s = set(Isa(i) for i in range(Isa.ARM.value, Isa.ARM32.value+1))
+Isa_ARMV6s = {Isa.ARMV6L, Isa.ARMV6M}
+Isa_ARMV7s = {Isa.ARMV7L, Isa.ARMV7M, Isa.ARMV7A, Isa.ARMV7R}
+Isa_POWERPC64s = {Isa.POWERPC, Isa.POWERPC64}
+Isa_MIPS64s = {Isa.MIPS, Isa.MIPS64}
+Isa_RISCV64s = {Isa.RISCV32, Isa.RISCV64}
+Isa_SPARC64s = {Isa.SPARC, Isa.SPARC64}
 
 
 # TODO: use hashmap
@@ -104,6 +124,7 @@ Isa_ARM32s = set(Isa(i) for i in range(Isa.ARM.value, Isa.ARM32.value+1))
 interfaces: dict[int, Interface] = dict()
 
 class Interface:
+    idx: int = 0
     def __init__(self,
             isa:    Isa     = Isa.NONE,
             kernel: Kernel  = Kernel.NONE,
@@ -112,6 +133,8 @@ class Interface:
             sysapp: Sysapp  = Sysapp.NONE,
             app:    App     = App.NONE,
             ) -> None:
+        self.idx = Interface.idx
+        Interface.idx += 1
         self.isa    = isa
         self.kernel = kernel
         self.syslib = syslib
@@ -121,12 +144,14 @@ class Interface:
         # higher, lower modules
         self.ls: set[IO] = set()
         self.hs: set[IO] = set()
-        self.repr = ''
-        for ele in (isa, kernel, syslib, lib, sysapp, app):
-            self.repr += ele.name[0] + '|'
+        strings = ()
+        for ele in (app, sysapp, lib, syslib, kernel, isa):
+            if ele.value > 1: # not NONE
+                strings += (ele.name,)
+        self.name = '-'.join(strings)
 
     def __repr__(self) -> str:
-        return self.repr
+        return self.name
 
     def __hash__(self) -> int:
         val = 0
@@ -199,10 +224,89 @@ class Metaface:
                                 ))
         return _interfaces
 
+    def __hash__(self) -> int:
+        val = 0
+        mul = 1
+        for isa in self.isas:
+            val ^= hash(isa.value * mul)
+        mul *= Isa.END.value
+        for kernel in self.kernels:
+            val ^= hash(kernel.value * mul)
+        mul *= Kernel.END.value
+        for syslib in self.syslibs:
+            val ^= hash(syslib.value * mul)
+        mul *= Syslib.END.value
+        for lib in self.libs:
+            val ^= hash(lib.value * mul)
+        mul *= Lib.END.value
+        for sysapp in self.sysapps:
+            val ^= hash(sysapp.value * mul)
+        mul *= Sysapp.END.value
+        for app in self.apps:
+            val ^= hash(app.value * mul)
+        mul *= App.END.value
+        return val
+
+    def __eq__(self, other: Metaface) -> bool:
+        if \
+                self.isas == other.isas and \
+                self.kernels == other.kernels and \
+                self.syslibs == other.syslibs and \
+                self.libs == other.libs and \
+                self.sysapps == other.sysapps and \
+                self.apps == other.apps:
+            return True
+        else:
+            return False
+
     def __repr__(self) -> str:
         return self.repr
 
+def addDummyModule(intfc: Interface) -> None:
+    name = intfc.isa.name
+    iargs = ()
+    oargs = ({intfc.isa},)
+    if name not in allios and \
+            intfc.isa.value > Isa.ANY.value:
+        DummyModule(name, {IO("", Metaface(*iargs), Metaface(*oargs))})
+    name = '-'.join((intfc.kernel.name, name))
+    iargs = oargs
+    oargs += ({intfc.kernel},)
+    if name not in allios and \
+            intfc.kernel.value > Kernel.ANY.value and \
+            intfc.isa.value > Isa.ANY.value:
+        DummyModule(name, {IO("", Metaface(*iargs), Metaface(*oargs))})
+    name = '-'.join((intfc.syslib.name, name))
+    iargs = oargs
+    oargs += ({intfc.syslib},)
+    if name not in allios and \
+            intfc.syslib.value > Syslib.NONE.value and \
+            intfc.kernel.value > Kernel.ANY.value:
+        DummyModule(name, {IO("", Metaface(*iargs), Metaface(*oargs))})
+    name = '-'.join((intfc.lib.name, name))
+    iargs = oargs
+    oargs += ({intfc.lib},)
+    if name not in allios and \
+            intfc.lib.value > Lib.NONE.value and \
+            intfc.syslib.value > Syslib.NONE.value:
+        DummyModule(name, {IO("", Metaface(*iargs), Metaface(*oargs))})
+    name = '-'.join((intfc.sysapp.name, name))
+    iargs = oargs
+    oargs += ({intfc.sysapp},)
+    if name not in allios and \
+            intfc.sysapp.value > Sysapp.NONE.value and \
+            intfc.lib.value > Lib.NONE.value:
+        DummyModule(name, {IO("", Metaface(*iargs), Metaface(*oargs))})
+    name = '-'.join((intfc.app.name, name))
+    iargs = oargs
+    oargs += ({intfc.app},)
+    if name not in allios and \
+            intfc.app.value > App.NONE.value and \
+            intfc.sysapp.value > Sysapp.NONE.value:
+        DummyModule(name, {IO("", Metaface(*iargs), Metaface(*oargs))})
+
 class IO:
+    idx: int = 0
     def __init__(self,
             name: str,
             i: Metaface,
@@ -210,6 +314,8 @@ class IO:
             # TODO: add efficiency
             # eff,
             ) -> None:
+        self.idx = IO.idx
+        IO.idx += 1
         self.name = name
         self.i = i
         self.o = o
@@ -228,10 +334,26 @@ class IO:
                 interfaces[hash] = intfc
             interfaces[hash].ls.add(self)
 
-    # make type checker happy,
-    # or type checker will alert self.module not exists
-    def _set_module(self, module: Module) -> None:
+    def setmodule_then_addio(self, module: Module) -> None:
         self.module = module
+        if len(self.name):
+            self.name = "-".join((module.name, self.name))
+        else:
+            self.name = module.name
+        if self.name in allios:
+            warnings.warn("io %s has been defined!" % self.name)
+        else:
+            allios[self.name] = self
+
+    def __hash__(self) -> int:
+        # make i and o not commutative
+        return hash(self.i.__hash__()) ^ self.o.__hash__()
+
+    def __eq__(self, other: IO) -> bool:
+        if self.i == other.i and self.o == other.o:
+            return True
+        else:
+            return False
 
     def __repr__(self) -> str:
         return self.repr
@@ -241,6 +363,8 @@ import datetime
 class Date(datetime.date):
     def __new__(cls, year: int = 1, month: int = 1, day: int = 1) -> Date:
         return super().__new__(cls, year, month, day)
+    def __str__(self) -> str:
+        return self.strftime("%Y.%m.%d")
 
 class Rename:
     def __init__(self,
@@ -256,9 +380,41 @@ class Module:
     def __init__(self,
             name: str,
             ios: set[IO],
+            ) -> None:
+        self.name = name
+        self.ios = ios
+        if name in modules and \
+                not isinstance(modules[name], DummyModule):
+            warnings.warn("module %s has been defined!" % name)
+        modules[name] = self
+        for io in ios:
+            io.setmodule_then_addio(modules[name])
+    def __repr__(self) -> str:
+        return self.name
+    def __hash__(self) -> int:
+        return hash(self.name)
+    def __eq__(self, other: Module) -> bool:
+        return self.name == other.name
+
+class DummyModule(Module):
+    def __init__(self,
+            name: str,
+            ios: set[IO],
+            ) -> None:
+        self.name = name
+        self.ios = ios
+        if name not in modules:
+            modules[name] = self
+        for io in ios:
+            io.setmodule_then_addio(modules[name])
+
+class Transor(Module):
+    def __init__(self,
+            name: str,
+            ios: set[IO],
             start: Date,
             stop: Date = Date(),
-            color: str = '',
+            color: str = '#000',
             # TODO: enum licenses
             license: str = '',
             # TODO: enum devs
@@ -267,41 +423,71 @@ class Module:
             desc: str = '',
             renames: list[Rename] = list(),
             ) -> None:
-        self.name = name
-        self.ios = ios
         self.start = start
-        self.stop = stop
+        if stop < start:
+            self.stop = start
+        else:
+            self.stop = stop
         self.color = color
         self.license = license
         self.dev = dev
         self.feat = feat
         self.desc = desc
         self.renames = renames
-        for io in ios:
-            io.module = self
-        modules[name] = self
+        super().__init__(name, ios)
 
-    def __repr__(self) -> str:
-        return self.name
 
-    def __hash__(self) -> int:
-        return hash(self.name)
-    def __eq__(self, other: Module) -> bool:
-        return self.name == other.name
+def addDummyModules() -> None:
+    _interfaces = interfaces.copy()
+    for intfc in _interfaces.values():
+        addDummyModule(intfc)
 
+def printDot() -> None:
+    def prefix(x: object) -> str:
+        return x.__class__.__name__[0]
+    print('''
+        digraph {
+            node[shape=box];
+    ''')
+    # nodes (IOs and Interfaces)
+    for io in allios.values():
+        ## Transors' IOs
+        if isinstance(io.module, Transor):
+            print('%s%d[label="%s", style=filled, fontcolor=white, fillcolor=black];' %(prefix(io.module), io.idx, io.name))
+        ## DummyModules' IOs
+        elif isinstance(io.module, DummyModule):
+            print('%s%d[label="%s", style="dotted"];' % (prefix(io.module), io.idx, io.name))
+        ## Other Modules' IOs
+        else:
+            print('%s%d[label="%s"];' %(prefix(io.module), io.idx, io.name))
+    ## Interfaces
+    for intfc in interfaces.values():
+        if len(intfc.name):
+            print('%s%d[label="%s", style=rounded];' %(prefix(intfc), intfc.idx, intfc.name))
+    # edges
+    for intfc in interfaces.values():
+        if len(intfc.name):
+            for l in intfc.ls:
+                print('%s%d -> %s%d' % (prefix(l.module), l.idx, prefix(intfc), intfc.idx))
+            for h in intfc.hs:
+                print('%s%d -> %s%d' % (prefix(intfc), intfc.idx, prefix(h.module), h.idx))
+    print("}")
 
 def printEdges() -> None:
+    print('digraph {')
     for intfc in interfaces.values():
-        for l in intfc.ls:
+        if len(intfc.name):
+            for l in intfc.ls:
+                print('"%s" -> "intfc:%s"' % (l.name, intfc.name))
             for h in intfc.hs:
-                print('"%s" -> "%s"' % (l.name, h.name))
+                print('"intfc:%s" -> "%s"' % (intfc.name, h.name))
+    print("}")
 
 def printGnucladCsv() -> None:
+    print("#, name, color, parent, start, stop, icon, desc, renames...")
     for module in modules.values():
-        print("#, name, color, parent, start, stop, icon, desc, renames...")
-        print('"%s","%s","%s","%s","%s","%s","%s","%s"' % (
-            "N", module.name, module.color, "",
-            module.start.strftime("%Y.%m.%d"),
-            module.stop.strftime("%Y.%m.%d"),
-            "", module.desc
+        if isinstance(module, Transor):
+            print('"%s","%s","%s","%s","%s","%s","%s","%s"' % (
+                "N", module.name, module.color, "",
+                module.start, module.stop, "", module.desc
             ))
