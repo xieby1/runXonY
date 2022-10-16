47b47a071ee3486e2b9a1c5c60ed33bf50dd6923
xieby1 <xieby1@outlook.com>
2022-10-16 14:27:38 +0800

    WINE:exagear as a connector, not a parent-child

--- scripts/data.py
+++ scripts/data.py
@@ -867,9 +867,10 @@ exagear_strategies = Transor("exagear strategies",
         Metaface({(Isa.ARM64, Up.USR)}, {Kernel.LINUX_ANDROID}, {Syslib.DEFAULT}, {Lib.ANY}, {Sysapp.ANY}, {App.ANDROID_RUNTIME}, {Rtlib.ANY}),
         Metaface(IsasUSR({Isa.X86, Isa.X86_64}), {Kernel.NO_SYSCALL}, {Syslib.WINDOWS}),
     )},
-    Date(2014), Date(2020), dev=Dev.ELTECH_RUSSIA, parent=WINE,
+    Date(2014), Date(2020), dev=Dev.ELTECH_RUSSIA,
     desc="https://4pda.to/forum/index.php?showtopic=992239",
 )
+Connector(exagear_strategies, WINE, Date(2014))
 
 Transor("WINE-Android",
     {  HG(isa.name,
