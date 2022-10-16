21c8e0f5fb1e47d628e2b1f87273eb44f9aad131
xieby1 <xieby1@outlook.com>
2022-10-16 14:23:47 +0800

    remove seaborn and qt in shell.nix

--- shell.nix
+++ shell.nix
@@ -1,19 +1,12 @@
 {}:
 let
   name = "runXonY";
-  pkgs = import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/78fd35f98e9411ddd2c31ef201a2fd8821bcd85a.tar.gz";
-    sha256 = "08l09d3i96arid2ag77lx715hzyf47ybb7cajcsp3iia24jpl6d8";
-  }) {};
+  pkgs = import <nixpkgs> {};
   myPython = pkgs.python3.withPackages (
     p: with p; [
       ipython
       multimethod
-      seaborn
-      # default tk cannot handle system zoom,
-      # qt5 will handle system zoom correctly,
-      # and ipython `%matplotlib qt` also needs,
-      pyqt5
+      matplotlib
     ]
   );
 in
@@ -21,7 +14,6 @@ pkgs.mkShell {
   inherit name;
   buildInputs = with pkgs; [
     myPython
-    qt5.full
   ];
   shellHook = ''
     # env
