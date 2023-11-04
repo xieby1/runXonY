be51866ba714b420c9466eafc15d857d623b3115
xieby1 <xieby1@outlook.com>
2023-11-04 16:58:21 +0800

    modularization: Merge

--- scripts/data.py
+++ scripts/data.py
@@ -171,15 +171,6 @@ Module("APPS-ANDROID", set(
 #    Date(),
 #)
 
-Merge = Transor("Merge",
-    {  HG("",
-        Metaface({(Isa.I386, Up.USR)}, {Kernel.SCO_UNIX}, {Syslib.DEFAULT}, {Lib.ANY}),
-        Metaface({(Isa.I386, Up.USR_PVL)})
-    )},
-    Date(1985,10,9), Date(2000),
-    feat="run DOS/Windows 3.1",
-    desc="https://en.wikipedia.org/wiki/Merge_(software)"
-)
 import os
 curfiledir = os.path.dirname(os.path.abspath(__file__))
 for dir in os.listdir(curfiledir):
