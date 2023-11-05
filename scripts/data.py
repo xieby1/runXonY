54a388ec07cd60d60495214b5a8d6116bda4feb8
xieby1 <xieby1@outlook.com>
2023-11-05 16:39:31 +0800

    modularization: WINE

--- scripts/data.py
+++ scripts/data.py
@@ -177,17 +177,6 @@ for dir in os.listdir(curfiledir):
     filepath = "/".join((curfiledir, dir, "meta.py"))
     if os.path.isfile(filepath):
         exec("from %s.meta import *" % dir)
-Kernel_WINEs_str: set[str] = {Kernel.LINUX.name, Kernel.MACOS.name, Kernel.BSD.name}
-Kernel_WINEs: set[Kernel] = set(Kernel[name] for name in Kernel_WINEs_str)
-Syslib_WINEs: set[Syslib] = set(Syslib[name] for name in Kernel_WINEs_str)
-WINE = Transor("WINE",
-    set(HG(
-        "-".join((kname, isa.name)),
-        Metaface({(isa, Up.USR)}, {Kernel[kname]}, {Syslib[kname]}, Lib_ANYs),
-        Metaface({(isa, Up.USR)}, {Kernel.NO_SYSCALL}, {Syslib.WINDOWS}),
-    ) for isa in Isa_MODERN_WINDOWSs for kname in Kernel_WINEs_str),
-    Date(1993,7,4), Date.today(), "#800000", "LGPL",
-)
 Transor("Cygwin",
     {  HG("",
         Metaface(IsasUSR(Isa_MODERN_WINDOWSs), {Kernel.WINDOWS}),
