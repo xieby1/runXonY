a309df65547a3fee460d72322eda5f30d488a51b
xieby1 <xieby1@outlook.com>
2023-11-05 19:07:22 +0800

    modularization: Digital Bridge 2

--- scripts/data.py
+++ scripts/data.py
@@ -178,20 +178,6 @@ for dir in os.listdir(curfiledir):
     if os.path.isfile(filepath):
         exec("from %s.meta import *" % dir)
 
-Transor("Digital Bridge 2",
-    {  HG("",
-        Metaface({(Isa.MIPS32, Up.USR)}, {Kernel.LINUX}, {Syslib.DEFAULT}, {Lib.ANY}),
-        Metaface({(Isa.X86, Up.USR)}, {Kernel.LINUX}),
-        term=Term.USER_LEVEL_BINARY_TRANSLATOR_WITH_LIB_PASS_THROUGH
-    )},
-    Date(2006), parent=Digital_Bridge, dev=Dev.WCG_LAB,
-    feat='''
-        Dynamic + static bt
-        lib wrapper
-    ''',
-    desc="2006: 二进制翻译中的库函数处理",
-)
-
 Transor("Linuxulator", set(
     HG(isa.name,
         Metaface({(isa, Up.USR)}, {Kernel.BSD}),
