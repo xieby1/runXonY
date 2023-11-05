3f7b5acf42b8b3a591ad6f15f0dddd818e546611
xieby1 <xieby1@outlook.com>
2023-11-05 23:03:06 +0800

    modularization: DeSmuME

--- scripts/data.py
+++ scripts/data.py
@@ -178,24 +178,6 @@ for dir in os.listdir(curfiledir):
     if os.path.isfile(filepath):
         exec("from %s.meta import *" % dir)
 
-# TODO: non-general, game
-Transor("DeSmuME",
-    {  HG("",
-        Metaface(IsasUSR({Isa.X86, Isa.X86_64}), {Kernel.WINDOWS, Kernel.LINUX, Kernel.MACOS}, {Syslib.DEFAULT}, {Lib.ANY}),
-        Metaface({(Isa.ARM32, Up.USR_PVL)}),
-    )},
-    Date(2006,4,6), Date.today(), "#A4A8FF", "GPL2",
-    feat='''
-        peripherals,
-        bios (loading and executing ROMs) (able to run external bios)
-    ''',
-    desc='''
-        https://desmume.org/
-        https://github.com/TASEmulators/desmume
-        http://wiki.desmume.org/index.php?title=Faq
-    ''',
-)
-
 Transor("playonlinux",
     {  HG("",
         Metaface(),
