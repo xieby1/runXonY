994bbcb40d4d466dcd6c4867ff05f9ebe15887a2
xieby1 <xieby1@outlook.com>
2023-11-05 23:01:18 +0800

    modularization: Rosetta

---
+++ scripts/Rosetta/meta.py
@@ -0,0 +1,10 @@
+from data import *
+
+Transor("Rosetta",
+    {  HG("",
+        Metaface({(Isa.X86, Up.USR)}, {Kernel.MACOS}, {Syslib.DEFAULT}, {Lib.ANY}),
+        Metaface({(Isa.POWERPC, Up.USR)}, {Kernel.MACOS}),
+    )},
+    Date(2006), Date(2011), "#525152", dev=Dev.APPLE,
+    desc="https://en.wikipedia.org/wiki/Rosetta_%28software%29",
+)
--- scripts/data.py
+++ scripts/data.py
@@ -178,15 +178,6 @@ for dir in os.listdir(curfiledir):
     if os.path.isfile(filepath):
         exec("from %s.meta import *" % dir)
 
-Transor("Rosetta",
-    {  HG("",
-        Metaface({(Isa.X86, Up.USR)}, {Kernel.MACOS}, {Syslib.DEFAULT}, {Lib.ANY}),
-        Metaface({(Isa.POWERPC, Up.USR)}, {Kernel.MACOS}),
-    )},
-    Date(2006), Date(2011), "#525152", dev=Dev.APPLE,
-    desc="https://en.wikipedia.org/wiki/Rosetta_%28software%29",
-)
-
 # TODO: non-general, game
 Transor("DeSmuME",
     {  HG("",
