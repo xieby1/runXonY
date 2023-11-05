51edcdbd0ba4685dd0d8e3ee6efafade36b37f34
xieby1 <xieby1@outlook.com>
2023-11-05 23:48:12 +0800

    modularization: QEMU_KVM

--- scripts/data.py
+++ scripts/data.py
@@ -178,16 +178,6 @@ for dir in os.listdir(curfiledir):
     if os.path.isfile(filepath):
         exec("from %s.meta import *" % dir)
 
-QEMU_KVM = Transor("QEMU-KVM",
-    {  HG("",
-        Metaface(IsasUSR({isa}), {Kernel.LINUX}, {Syslib.DEFAULT}, {Lib.ANY}),
-        Metaface(IsasUSR_PVL({isa})),
-    ) for isa in Isa_KVMs},
-    Date(2008,11,6), Date.today(), color="#F60", parent=QEMU_sys,
-    desc="Git commit: 7ba1e61953f459: Add KVM support to QEMU",
-)
-Connector(KVM, QEMU_KVM, Date(2008,11,6))
-
 Transor("DistriBit",
     {  HG("",
         Metaface(),
