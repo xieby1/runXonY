832812aafa170b744358b4ec7b28015e95bf84c5
xieby1 <xieby1@outlook.com>
2023-11-05 17:46:32 +0800

    modularization: DAISY

--- scripts/data.py
+++ scripts/data.py
@@ -177,20 +177,6 @@ for dir in os.listdir(curfiledir):
     filepath = "/".join((curfiledir, dir, "meta.py"))
     if os.path.isfile(filepath):
         exec("from %s.meta import *" % dir)
-Transor("DAISY",
-    {  HG("",
-        Metaface({(Isa.DAISY_VLIW, Up.USR_PVL)}),
-        Metaface({(Isa.POWERPC, Up.USR_PVL)}),
-    )},
-    Date(1998), Date(2001), dev=Dev.IBM,
-    desc = '''
-        1997: DAISY dynamic compilation for 100% architecutral compatibility
-        2000: Binary Translation and Architecture Convergence Issues for IBM System/390
-        2000: full system binary translation RISC to VLIW
-        2000: simulation and debugging of full system bianry translation
-        2001: Dynamic binary translation and optimization
-    '''
-)
 Transor("MinGW",
     {  HG("",
         Metaface(IsasUSR(Isa_MODERN_WINDOWSs), {Kernel.WINDOWS}),
