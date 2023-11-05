dbba52c8cee74ab6e96bb14bb48f1f6617648c5e
xieby1 <xieby1@outlook.com>
2023-11-05 23:51:54 +0800

    modularization: Denver

---
+++ scripts/Denver/meta.py
@@ -0,0 +1,10 @@
+from data import *
+
+Transor("Denver",
+    {  HG("",
+        Metaface({(Isa.DENVER, Up.USR_PVL)}),
+        Metaface({(Isa.ARM64, Up.USR_PVL)}),
+    )},
+    Date(2011,3,4), Date(2014,10,15), dev=Dev.NVIDIA,
+    desc="https://en.wikipedia.org/wiki/Project_Denver",
+)
--- scripts/data.py
+++ scripts/data.py
@@ -178,15 +178,6 @@ for dir in os.listdir(curfiledir):
     if os.path.isfile(filepath):
         exec("from %s.meta import *" % dir)
 
-Transor("Denver",
-    {  HG("",
-        Metaface({(Isa.DENVER, Up.USR_PVL)}),
-        Metaface({(Isa.ARM64, Up.USR_PVL)}),
-    )},
-    Date(2011,3,4), Date(2014,10,15), dev=Dev.NVIDIA,
-    desc="https://en.wikipedia.org/wiki/Project_Denver",
-)
-
 Transor("BPF(JIT)",
     {  HG("",
         Metaface(IsasUSR(Isa_LINUXs), {Kernel.LINUX}),
