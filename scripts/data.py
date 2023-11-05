4a8a1c969aaa18a9cf9834e14004ad2938708586
xieby1 <xieby1@outlook.com>
2023-11-05 16:30:30 +0800

    modularization: Shade

--- scripts/data.py
+++ scripts/data.py
@@ -177,28 +177,6 @@ for dir in os.listdir(curfiledir):
     filepath = "/".join((curfiledir, dir, "meta.py"))
     if os.path.isfile(filepath):
         exec("from %s.meta import *" % dir)
-Transor("Shade",
-    {  HG("MIPS.V8",
-        Metaface({(Isa.SPARCV8, Up.USR)}, {Kernel.SUNOS4_BSD}, {Syslib.DEFAULT}, Lib_ANYs),
-        Metaface({(Isa.UMIPSV, Up.USR), (Isa.MIPSI, Up.USR)}, {Kernel.SUNOS4_BSD}),
-    ), HG("V8.V8-BSD",
-        Metaface({(Isa.SPARCV8, Up.USR)}, {Kernel.SUNOS4_BSD}, {Syslib.DEFAULT}, Lib_ANYs),
-        Metaface({(Isa.SPARCV8, Up.USR)}, {Kernel.SUNOS4_BSD}),
-        term=Term.USER_LEVEL_BINARY_TRANSLATOR,
-    ), HG("V9.V8-BSD",
-        Metaface({(Isa.SPARCV8, Up.USR)}, {Kernel.SUNOS4_BSD}, {Syslib.DEFAULT}, Lib_ANYs),
-        Metaface({(Isa.SPARCV9, Up.USR)}, {Kernel.SUNOS4_BSD}),
-    ), HG("V8.V8-UNIX",
-        Metaface({(Isa.SPARCV8, Up.USR)}, {Kernel.SUNOS5_UNIX}, {Syslib.DEFAULT}, Lib_ANYs),
-        Metaface({(Isa.SPARCV8, Up.USR)}, {Kernel.SUNOS5_UNIX}),
-        term=Term.USER_LEVEL_BINARY_TRANSLATOR,
-    ), HG("V9.V8-UNIX",
-        Metaface({(Isa.SPARCV8, Up.USR)}, {Kernel.SUNOS5_UNIX}, {Syslib.DEFAULT}, Lib_ANYs),
-        Metaface({(Isa.SPARCV9, Up.USR)}, {Kernel.SUNOS5_UNIX}),
-    )},
-    Date(1993), dev=Dev.SUN,
-    desc="1994: Shade: a fast instruction-set simulator for execution profiling"
-)
 Transor("VEST",
     {  HG("",
         Metaface({(Isa.ALPHA, Up.USR)}, {Kernel.OPENVMS}, {Syslib.DEFAULT}, Lib_ANYs),
