302980fdbfe94a88529d2abc394a90e3b8746b45
xieby1 <xieby1@outlook.com>
2023-11-05 23:55:40 +0800

    modularization: LLBT

--- scripts/data.py
+++ scripts/data.py
@@ -178,20 +178,6 @@ for dir in os.listdir(curfiledir):
     if os.path.isfile(filepath):
         exec("from %s.meta import *" % dir)
 
-Transor("LLBT",
-    {  HG("",
-        Metaface({(Isa.LLVM, Up.USR)}, {Kernel.LINUX}),
-        Metaface({(Isa.ARM, Up.USR)}, {Kernel.LINUX}),
-        term=Term.STATIC_BINARY_TRANSLATOR,
-    )},
-    Date(2012), Date(2014), dev=Dev.TJD,
-    desc='''
-        2012: LLBT: An LLVM-based Static Binary Translator
-        2013: Automatic Validation for Static Binary Translation
-        2014: A Retargetable Static Binary Translator for the ARM Architecture
-    ''',
-)
-
 # TODO: non-general, game
 Transor("PPSSPP",
     {  HG("",
