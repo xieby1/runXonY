6957b420a0727a6fd5c5b46cc26b5553450e7039
xieby1 <xieby1@outlook.com>
2023-11-05 18:40:30 +0800

    modularization: Project David

--- scripts/data.py
+++ scripts/data.py
@@ -178,19 +178,6 @@ for dir in os.listdir(curfiledir):
     if os.path.isfile(filepath):
         exec("from %s.meta import *" % dir)
 
-Transor("Project David",
-    {  HG("",
-        Metaface(),
-        Metaface(),
-    )},
-    Date(2004), Date(2004), dev=Dev.SPECOPS_LABS,
-    desc='''
-        Mail from wine-devel: 2004-April.txt: project David
-        Turbo Linux: https://web.archive.org/web/20051201013454/http://digital.hmx.net/02contents/pc/linux/fuji.shtml
-        http://www.specopslabs.com/david.htm
-    ''',
-    parent=WINE,
-)
 
 Digital_Bridge = Transor("Digital Bridge",
     {  HG("",
