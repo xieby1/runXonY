14b4e6e9f1c0c6e0579f497f5e54e5748b37923b
xieby1 <xieby1@outlook.com>
2023-11-05 17:34:04 +0800

    modularization: FX!32

---
+++ scripts/FX32/meta.py
@@ -0,0 +1,13 @@
+from data import *
+
+Transor("FX!32",
+    {  HG("",
+        Metaface({(Isa.ALPHA, Up.USR)}, {Kernel.WINDOWS_NT4_0}, {Syslib.DEFAULT}, Lib_ANYs),
+        Metaface({(Isa.X86, Up.USR)}, {Kernel.WINDOWS_NT4_0}),
+    )},
+    Date(1996), dev=Dev.DIGITAL,
+    desc = '''
+        1997: DIGITAL FX!32: Combining Emulation and Binary Translation
+        1998: FX!32 A Profile-Directed Binary Translator
+    '''
+)
--- scripts/data.py
+++ scripts/data.py
@@ -177,17 +177,6 @@ for dir in os.listdir(curfiledir):
     filepath = "/".join((curfiledir, dir, "meta.py"))
     if os.path.isfile(filepath):
         exec("from %s.meta import *" % dir)
-Transor("FX!32",
-    {  HG("",
-        Metaface({(Isa.ALPHA, Up.USR)}, {Kernel.WINDOWS_NT4_0}, {Syslib.DEFAULT}, Lib_ANYs),
-        Metaface({(Isa.X86, Up.USR)}, {Kernel.WINDOWS_NT4_0}),
-    )},
-    Date(1996), dev=Dev.DIGITAL,
-    desc = '''
-        1997: DIGITAL FX!32: Combining Emulation and Binary Translation
-        1998: FX!32 A Profile-Directed Binary Translator
-    '''
-)
 Transor("DAISY",
     {  HG("",
         Metaface({(Isa.DAISY_VLIW, Up.USR_PVL)}),
