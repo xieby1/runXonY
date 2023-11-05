a9bb866bea0dde15dd0d1bc9191e73a87c1ad90c
xieby1 <xieby1@outlook.com>
2023-11-05 23:51:14 +0800

    modularization: DOSBox-X

--- scripts/data.py
+++ scripts/data.py
@@ -178,17 +178,6 @@ for dir in os.listdir(curfiledir):
     if os.path.isfile(filepath):
         exec("from %s.meta import *" % dir)
 
-Transor("DOSBox-X",
-    {  HG("",
-        Metaface(IsasUSR(isas), {kernel}, {Syslib.DEFAULT}, {Lib.ANY}),
-        Metaface({(Isa.X86, Up.USR_PVL)}),
-        term=Term.V2B,
-    ) for isas,kernel in zip(
-        [Isa_MODERN_WINDOWSs, Isa_MODERN_MACOSs, Isa_LINUXs, Isa_BSDs],
-        [Kernel.WINDOWS, Kernel.MACOS, Kernel.LINUX, Kernel.BSD])},
-    Date(2011), Date.today(), color="#113466", parent=DOSBox,
-)
-
 Transor("Denver",
     {  HG("",
         Metaface({(Isa.DENVER, Up.USR_PVL)}),
