3afa0645fa647829cdd045eccd6acbcd6db07c72
xieby1 <xieby1@outlook.com>
2023-11-05 18:21:01 +0800

    modularization: DOSBox

--- scripts/data.py
+++ scripts/data.py
@@ -177,18 +177,6 @@ for dir in os.listdir(curfiledir):
     filepath = "/".join((curfiledir, dir, "meta.py"))
     if os.path.isfile(filepath):
         exec("from %s.meta import *" % dir)
-Isa_DOSBOXs = {Isa.X86_64, Isa.X86, Isa.MIPS32, Isa.ARM32, Isa.POWERPC}
-DOSBox = Transor("DOSBox",
-    {  HG("",
-        Metaface(IsasUSR(isas), {kernel}, {Syslib.DEFAULT}, {Lib.ANY}),
-        Metaface({(Isa.X86, Up.USR_PVL)}),
-        term=Term.V2B,
-    ) for isas,kernel in zip(
-        [Isa_MODERN_WINDOWSs, Isa_MODERN_MACOSs, Isa_LINUXs, Isa_BSDs],
-        [Kernel.WINDOWS, Kernel.MACOS, Kernel.LINUX, Kernel.BSD])},
-    Date(2002), Date.today(), "#113466",
-    desc="https://sourceforge.net/projects/dosbox/",
-)
 # TODO:
 LLVM = Transor("LLVM",
     {  HG("",
