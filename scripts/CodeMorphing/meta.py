8206542e68640c9d8eb824eb464bbb6796d44732
xieby1 <xieby1@outlook.com>
2023-11-05 18:11:22 +0800

    modularization: Code Morphing

---
+++ scripts/CodeMorphing/meta.py
@@ -0,0 +1,13 @@
+from data import *
+
+Transor("Code Morphing",
+    {  HG("",
+        Metaface({(Isa.CRUSOE_VLIW, Up.USR_PVL)}),
+        Metaface({(Isa.X86, Up.USR_PVL)}),
+    )},
+    Date(2000), Date(2009), dev=Dev.TRANSMETA,
+    desc='''
+        2003: The Transmeta Code Morphing Software: Using Speculation, Recovery, and Adaptive Retranslation to Address Real-Life Challenges
+        https://en.wikipedia.org/wiki/Transmeta
+    ''',
+)
--- scripts/data.py
+++ scripts/data.py
@@ -177,17 +177,6 @@ for dir in os.listdir(curfiledir):
     filepath = "/".join((curfiledir, dir, "meta.py"))
     if os.path.isfile(filepath):
         exec("from %s.meta import *" % dir)
-Transor("Code Morphing",
-    {  HG("",
-        Metaface({(Isa.CRUSOE_VLIW, Up.USR_PVL)}),
-        Metaface({(Isa.X86, Up.USR_PVL)}),
-    )},
-    Date(2000), Date(2009), dev=Dev.TRANSMETA,
-    desc='''
-        2003: The Transmeta Code Morphing Software: Using Speculation, Recovery, and Adaptive Retranslation to Address Real-Life Challenges
-        https://en.wikipedia.org/wiki/Transmeta
-    ''',
-)
 Transor("Aries",
     {  HG("",
         Metaface({(Isa.IA64, Up.USR)}, {Kernel.UNIX}, {Syslib.DEFAULT}, {Lib.ANY}),
