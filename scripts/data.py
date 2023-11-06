8aafebefcd8b5b7788a05cf57aade4bf311a05b3
xieby1 <xieby1@outlook.com>
2023-11-06 12:05:26 +0800

    modularization: box64

--- scripts/data.py
+++ scripts/data.py
@@ -178,24 +178,6 @@ for dir in os.listdir(curfiledir):
     if os.path.isfile(filepath):
         exec("from %s.meta import *" % dir)
 
-Transor("box64",
-    {  HG("DynaRec",
-        Metaface({(Isa.ARM64, Up.USR)}, {Kernel.LINUX}, {Syslib.DEFAULT}, {Lib.ANY}),
-        Metaface({(Isa.X86_64, Up.USR)}, {Kernel.LINUX}),
-        term=Term.USER_LEVEL_BINARY_TRANSLATOR_WITH_LIB_PASS_THROUGH,
-        perfs=[
-            Perf(3763.83/10024.33, Benchmark.COREMARK, Date(2023,1,15),
-                 "box64 0.1.8, x86_64 on raspberrypi4, gcc 11.3.0"),
-        ],
-    ), HG("without-DynaRec",
-        Metaface(IsasUSR({Isa.ARM64, Isa.LA64, Isa.RISCV64, Isa.POWERPC64, Isa.X86_64}), {Kernel.LINUX}),
-        Metaface({(Isa.X86_64, Up.USR)}, {Kernel.LINUX}),
-        term=Term.USER_LEVEL_BINARY_TRANSLATOR_WITH_LIB_PASS_THROUGH,
-    )},
-    Date(2020,12,1), Date.today(), color="#B5E853", parent=box86,
-    desc="https://github.com/ptitSeb/box64",
-)
-
 Anbox_halium = Transor("Anbox-halium",
     {  HG("",
         Metaface(),
