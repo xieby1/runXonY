4b9afcf3434b45424713e08afe4d9bb2710d6a91
xieby1 <xieby1@outlook.com>
2023-11-06 11:45:38 +0800

    modularization: box86

--- scripts/data.py
+++ scripts/data.py
@@ -178,17 +178,6 @@ for dir in os.listdir(curfiledir):
     if os.path.isfile(filepath):
         exec("from %s.meta import *" % dir)
 
-box86 = Transor("box86",
-    {  HG("",
-        Metaface({(Isa.ARM32, Up.USR)}, {Kernel.LINUX}, {Syslib.DEFAULT}, {Lib.ANY}),
-        Metaface({(Isa.X86, Up.USR)}, {Kernel.LINUX}),
-        term=Term.USER_LEVEL_BINARY_TRANSLATOR_WITH_LIB_PASS_THROUGH,
-    )},
-    Date(2018), Date.today(), color="#B5E853",
-    feat="lib warp",
-    desc="https://github.com/ptitSeb/box86",
-)
-
 # non-general game
 Ryujinx = Transor("Ryujinx",
     {  HG("",
