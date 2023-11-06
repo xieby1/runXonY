2973a1dcf12052e4d7aa4d770c64a82b1f2c6bf9
xieby1 <xieby1@outlook.com>
2023-11-06 11:58:50 +0800

    modularization: ARMeilleure

--- scripts/data.py
+++ scripts/data.py
@@ -178,16 +178,6 @@ for dir in os.listdir(curfiledir):
     if os.path.isfile(filepath):
         exec("from %s.meta import *" % dir)
 
-Transor("ARMeilleure",
-    {  HG("",
-        Metaface(IsasUSR({Isa.X86_64}), {Kernel.WINDOWS, Kernel.LINUX}, {Syslib.DEFAULT}, {Lib.ANY}),
-        Metaface({(Isa.ARM64, Up.USR)}),
-    )},
-    Date(2019,8,9), Date.today(), color="#FF5F55", parent=Ryujinx,
-    feat="Framework",
-    desc="https://github.com/Ryujinx/Ryujinx/",
-)
-
 Transor("Rosetta 2",
     {  HG("",
         Metaface({(Isa.AARCH64, Up.USR)}, {Kernel.MACOS}, {Syslib.DEFAULT}, {Lib.ANY}),
