f5b7d55fd2694f9815da11e5a0f1a5f534e8a2a4
xieby1 <xieby1@outlook.com>
2023-11-06 11:38:45 +0800

    modularization: MAMBO_X64

--- scripts/data.py
+++ scripts/data.py
@@ -178,16 +178,6 @@ for dir in os.listdir(curfiledir):
     if os.path.isfile(filepath):
         exec("from %s.meta import *" % dir)
 
-MAMBO_X64 = Transor("MAMBO-X64",
-    {  HG("",
-        Metaface({(Isa.ARM64, Up.USR)}, {Kernel.LINUX}, {Syslib.DEFAULT}, {Lib.ANY}),
-        Metaface({(Isa.ARM32, Up.USR)}, {Kernel.LINUX}),
-    )},
-    Date(2017), Date.today(), dev=Dev.MANCHESTER,
-    desc='''
-        2017: Low Overhead Dynamic Binary Translation on ARM
-    ''',
-)
 HyperMAMBO_X64 = Transor("HyperMAMBO-X64",
     {  HG("",
         Metaface({(Isa.ARM64, Up.USR)}, {Kernel.LINUX}, {Syslib.DEFAULT}, {Lib.ANY}),
