f1f5b557e5b156443ffb059e9a3ac6b678558974
xieby1 <xieby1@outlook.com>
2023-11-06 11:44:07 +0800

    modularization: multipass

--- scripts/data.py
+++ scripts/data.py
@@ -178,18 +178,6 @@ for dir in os.listdir(curfiledir):
     if os.path.isfile(filepath):
         exec("from %s.meta import *" % dir)
 
-multipass = Transor("multipass",
-    {  HG("",
-        Metaface({(Isa.X86_64, Up.USR)}, {Kernel.WINDOWS, Kernel.MACOS, Kernel.LINUX}, {Syslib.DEFAULT}, {Lib.ANY}),
-        Metaface({(Isa.X86_64, Up.USR)}, {Kernel.LINUX}),
-        term=Term.V2_,
-    )},
-    Date(2017,12,7), Date.today(), color="#E95420", license="GPL3", dev=Dev.CANONICAL,
-    desc="https://github.com/canonical/multipass",
-)
-# TODO: windows:hyper-v, macos:hyperkit
-Connector(KVM, multipass, Date(2017,12,7))
-
 box86 = Transor("box86",
     {  HG("",
         Metaface({(Isa.ARM32, Up.USR)}, {Kernel.LINUX}, {Syslib.DEFAULT}, {Lib.ANY}),
