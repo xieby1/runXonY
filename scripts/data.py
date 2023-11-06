164941743abb7762933924778dea9ad792a4f8e9
xieby1 <xieby1@outlook.com>
2023-11-06 12:08:15 +0800

    modularization: waydroid

--- scripts/data.py
+++ scripts/data.py
@@ -178,16 +178,5 @@ for dir in os.listdir(curfiledir):
     if os.path.isfile(filepath):
         exec("from %s.meta import *" % dir)
 
-Transor("waydroid",
-    {  HG("",
-        Metaface({(isa, Up.USR)}, {Kernel.LINUX}, {Syslib.DEFAULT}, {Lib.ANY}),
-        Metaface({(isa, Up.USR)}, {Kernel.LINUX_ANDROID}, {Syslib.DEFAULT}, {Lib.ANY}, {Sysapp.ANY}, {App.ANDROID_RUNTIME}),
-    ) for isa in [Isa.X86_64, Isa.ARM64]},
-    Date(2021,8,27), Date.today(), color="#F0AD4E", parent=Anbox_halium,
-    desc='''
-        https://www.reddit.com/r/Ubports/comments/oovy13/anbox_for_halium9_aka_new_anbox_update/
-        https://github.com/waydroid/waydroid
-    ''',
-)
 # Lastly, add dummy modules after all modules are added
 addDummyModules()
